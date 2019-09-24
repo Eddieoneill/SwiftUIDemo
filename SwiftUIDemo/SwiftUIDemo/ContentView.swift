23699154a53177cc50a44ddb29b67327fa410e19
Edward O'Neill <edwardoneill@Edwards-MacBook-Pro.local>
2019-09-24 15:51:56 -0400

    added xcode project

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by Edward O'Neill on 9/24/19.
+//  Copyright © 2019 Eddie O'Neill. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello There!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
